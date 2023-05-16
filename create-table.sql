56f3b7a83e1f0d4bcb0c5f43a84ece2f62286688
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-05-17 00:17:16 +0700

    finish exercise

--- create-table.sql
+++ create-table.sql
@@ -1,1 +1,11 @@
--- TODO: answer here
+CREATE TABLE persons (
+    id INTEGER NOT NULL PRIMARY KEY,
+    NIK varchar(255) NOT NULL UNIQUE,
+    fullname varchar(255) NOT NULL,
+    gender varchar(50) NOT NULL,
+    birth_date DATE NOT NULL,
+    is_married bool,
+    height FLOAT,
+    weight FLOAT,
+    address TEXT
+)
